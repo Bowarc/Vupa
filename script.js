6fedef42fec7123f28274806acda101f5d463798
Bowarc <yuyeaaaahhhh@laposte.net>
2021-11-10 23:10:02 +0100

    adding js

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("It's working");
+let a = 0;
+
+function sleep(sleepDuration){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* Do nothing */ }
+}
+console.log("salut");
+// sleep(1000);
+console.log("salut");
+// while (true) {
+//     console.log(a);
+//     a = a + 1;
+//     if (a > 100) {
+//         break;
+//     }
+    
+// }
+
+function CustomFunc() {
+    console.log("salut");
+    // let obj = document.getElementById("title");
+    document.body.style.background = "red"
+}
