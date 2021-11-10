b43c9000508115e2e88f4222672f9fea90f38a9c
Bowarc <yuyeaaaahhhh@laposte.net>
2021-11-10 23:26:34 +0100

    update

--- script.js
+++ script.js
@@ -23,16 +23,16 @@ function CustomFunc() {
 }
 
 function showTime(){
+    // https://codepen.io/afarrar/pen/JRaEjP
     var date = new Date();
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
     var session = "AM";
     
-    if(h == 0){
-        h = 12;
-    }
-    
+    // if(h == 0){
+    //     h = 12;
+    // }
     // if(h > 12){
     //     h = h - 12;
     //     session = "PM";
