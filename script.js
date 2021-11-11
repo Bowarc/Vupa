9342a025f8f1c13b40f24c1a8e84c4077c20a451
Bowarc <yuyeaaaahhhh@laposte.net>
2021-11-11 23:29:17 +0100

    adding title animation

--- script.js
+++ script.js
@@ -16,38 +16,52 @@ console.log("This is a test 2");
 //     }
 // }
 
-function CustomFunc() {
-    console.log("Hey");
-    let obj = document.getElementById("title");
-    obj.style.color = "red";
-}
-
 function showTime(){
     // https://codepen.io/afarrar/pen/JRaEjP
     var date = new Date();
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    
-    // if(h == 0){
-    //     h = 12;
-    // }
-    // if(h > 12){
-    //     h = h - 12;
-    //     session = "PM";
-    // }
+    // var session = "AM";
     
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
-    var time = h + ":" + m + ":" + s + " ";
+    var time = h + ":" + m + ":" + s;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
     
-    setTimeout(showTime, 1000);
-    
+    setTimeout(showTime, 1000);    
 }
 
+function AnimateTitle(phase1, phase2){
+    let wanted_title = "Awopa";
+    let text = wanted_title.slice(phase2,phase1);
+
+    let obj = document.getElementById("title");
+    if (text == ""){
+        text = "u"
+        console.log("yes")
+    }
+    let style = window.getComputedStyle(obj, null)
+    obj.textContent  = text;
+    console.log(style)
+    obj.style = style;
+    // document.getElementById("title").textContent = text;
+
+    if (phase1 > wanted_title.length - 1){
+        phase2 = phase2 + 1;
+        if (phase2 > wanted_title.length - 1){
+            phase1 = 0;
+            phase2 = 0;
+        }
+    }else{
+        phase1 = phase1 + 1;
+    }
+
+    setTimeout(AnimateTitle.bind(null, phase1, phase2), 500);
+}
+
+// AnimateTitle(0,0);
 showTime();
---
+++ snake.py
@@ -0,0 +1,3 @@
+# nbr = 0
+for i in range(20):
+    print(f'<div class="dot" style="--i:{i}"></div>')
